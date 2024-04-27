1a8035fe0c6b2b27f7f35e89300bbf3100e3d9ee
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2024-04-27 20:12:55 +0100

    Clean up resources after closing Pull request (#11)

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,30 @@ import * as core from "@actions/core"
 import { context } from "@actions/github"
 
 import { createCloudfront } from "./aws/cloudfront"
-import { setupS3Bucket, syncFiles, updateBucketPolicy } from "./aws/s3"
-import { createRoute53Record } from "./aws/route53"
-import { startDeployment, updateDeploymentStatus } from "./github/deployments"
+import {
+  setupS3Bucket,
+  syncFiles,
+  updateBucketPolicy,
+  deleteObjects,
+} from "./aws/s3"
+import { createRoute53Record, deleteRoute53Record } from "./aws/route53"
+import {
+  startDeployment,
+  updateDeploymentStatus,
+  deleteDeployments,
+} from "./github/deployments"
 import { getAppName, getBuidDir, getDomainName } from "./config"
 
 type CreateAwsResourcesInputParams = {
   bucketName: string
   domainName: string
-  previewSubDomain: string
+  environment: string
 }
 
 async function createAwsResources({
   bucketName,
   domainName,
-  previewSubDomain,
+  environment,
 }: CreateAwsResourcesInputParams) {
   // TODO: Ability to give custom region for S3 bucket
   const originId = `${bucketName}.s3.us-east-1.amazonaws.com`
@@ -26,11 +35,68 @@ async function createAwsResources({
 
   await createRoute53Record({
     domainName,
-    recordName: `${previewSubDomain}.${domainName}`,
+    recordName: `${environment}.${domainName}`,
     routeTrafficTo: cloudfront.domainName,
   })
 
   return cloudfront
+}
+
+type EnvironmentActionParams = {
+  appName: string
+  domainName: string
+  environment: string
+  bucketName: string
+  branchName: string
+  pullRequestNumber: number
+}
+
+async function createPreviewEnvironment({
+  domainName,
+  environment,
+  bucketName,
+  branchName,
+}: EnvironmentActionParams) {
+  const buildDir = getBuidDir()
+  const previewUrl = `https://${environment}.${domainName}`
+
+  await createAwsResources({
+    bucketName,
+    domainName,
+    environment,
+  })
+
+  const deploymentId = await startDeployment(environment, branchName)
+  await syncFiles({
+    bucketName,
+    prefix: environment,
+    directory: buildDir,
+  })
+
+  // Deployments are not failing Github action if anything goes wrong during creation
+  if (deploymentId)
+    await updateDeploymentStatus({
+      deploymentId,
+      status: "success",
+      previewUrl,
+      environment,
+    })
+
+  core.setOutput("url", previewUrl)
+}
+
+async function deletePreviewEnvironment({
+  domainName,
+  environment,
+  bucketName,
+  branchName,
+}: EnvironmentActionParams) {
+  await deleteObjects(bucketName, environment)
+  await deleteDeployments(environment, branchName)
+  await deleteRoute53Record({
+    domainName: getDomainName(),
+    recordName: `${environment}.${domainName}`,
+  })
 }
 
 /**
@@ -39,48 +105,46 @@ async function createAwsResources({
  */
 export async function run(): Promise<void> {
   try {
-    const buildDir = getBuidDir()
+    const { action } = context.payload
+    const pullRequest = context.payload.pull_request
+    if (!pullRequest) {
+      throw new Error(
+        "This action can only be run on pull requests. Exiting...",
+      )
+    }
+
     const appName = getAppName()
     const domainName = getDomainName()
-    const pullRequestNumber = context.payload.pull_request?.number
+    const pullRequestNumber = pullRequest.number
     const environment = pullRequestNumber
       ? `preview-${pullRequestNumber}`
       : "preview"
     const bucketName = `${appName}-preview-deployment`
-    const previewUrl = `https://${environment}.${domainName}`
+    const branchName = pullRequest.head.ref
 
-    console.log("Input Params", {
-      buildDir,
+    const params = {
       appName,
       domainName,
+      environment,
+      bucketName,
+      branchName,
       pullRequestNumber,
-      previewSubDomain: environment,
-      bucketName,
-    })
+    }
 
-    await createAwsResources({
-      bucketName,
-      domainName,
-      previewSubDomain: environment,
-    })
+    console.log("Running action with params", params)
 
-    const deploymentId = await startDeployment(environment)
-    await syncFiles({
-      bucketName,
-      prefix: environment,
-      directory: buildDir,
-    })
-
-    // Deployments are not failing Github action if anything goes wrong during creation
-    if (deploymentId)
-      await updateDeploymentStatus({
-        deploymentId,
-        status: "success",
-        previewUrl,
-        environment,
-      })
-
-    core.setOutput("url", previewUrl)
+    switch (action) {
+      case "opened":
+      case "reopened":
+      case "synchronize":
+        await createPreviewEnvironment(params)
+        break
+      case "closed":
+        await deletePreviewEnvironment(params)
+        break
+      default:
+        throw new Error(`${action} is not implemented...`)
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
